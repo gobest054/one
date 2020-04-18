f836cdc25a796551958dd2b72e4cac48a0f154fc
GoBeSt <kjn582923@gmail.com>
2020-04-18 16:18:55 +0700

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+import com.sun.deploy.security.SelectableSecurityManager;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        for (int i =1000000 ; i >= 1; i--) {
+            System.out.println(i);
+        }
+    }
+}
